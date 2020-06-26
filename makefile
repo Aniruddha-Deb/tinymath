95ed0ed818ace9ed418bf228f4d8db4a708e0004
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2020-06-26 19:23:53 +0530

    Improved makefile and added a test folder for tests

--- makefile
+++ makefile
@@ -1,5 +1,13 @@
 CC = gcc
 CFLAGS = -Wall -std=c99 -lm
 
-tinymath_test: tinymath_test.c tinymath.h
-	$(CC) $(CFLAGS) -o obj/tmath_test tinymath_test.c
+TESTS = $(wildcard test/*.c)
+TEST_OUTPUTS = $(patsubst test/%.c,obj/test/%, $(TESTS))
+
+.PHONY: test
+
+test: 
+	$(foreach file, $(TESTS), \
+		$(CC) $(CFLAGS) -o obj/$(patsubst test/%.c,test/%, $(file)) $(file); \
+		chmod +x obj/$(patsubst test/%.c,test/%, $(file)); )
+	$(foreach file, $(TEST_OUTPUTS), $(file);)
---
+++ test/tinymath_test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../tinymath.h"
+
+int main(int argc, char** argv) {
+	float f1 = 6;
+	float f2 = 4;
+	if (argc > 1) {
+		f1 = atof(argv[1]);
+	}
+	if (argc > 2) {
+		f2 = atof(argv[2]);
+	}
+
+	fixed a = FLOAT_TO_FIXED(f1);
+	print_fixed("a = ", a);
+	fixed b = FLOAT_TO_FIXED(f2);
+	print_fixed("b = ", b);
+
+	print_fixed("sqrt(a) = ", sqrt_fast_fixed(a));
+	print_fixed("ln(a) = ", ln_fast_fixed(a));
+	print_fixed("pow(a,b) = ", pow_fixed(a,b));
+	return 0;
+}
